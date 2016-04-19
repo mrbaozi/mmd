b31c1e6e4ca9fb098ec3079f59d1286849df3bd0
mrbaozi <mrbaozi@me.com>
2016-04-19 22:42:21 +0200

    Finish a1_9

--- sheet1/a1_9.py
+++ sheet1/a1_9.py
@@ -1,16 +1,40 @@
 #!/usr/local/bin/python3
 
 import sys
+from pprint import pprint
 import numpy as np
 import matplotlib.pyplot as plt
 
-N = int(sys.argv[1])
-MU1, SIGMA1 = 2, 1.5
-MU2, SIGMA2 = 3, 2.2
+if __name__ == "__main__":
 
-X1 = np.random.normal(MU1, SIGMA1, N)
-X2 = np.random.normal(MU2, SIGMA2, N)
+    try:
+        N = int(sys.argv[1])
+    except IndexError:
+        N = 100
 
-plt.hist(X1/X2, 50, normed=True)
+    # sheet values
+    MU1, SIGMA1 = 2, 1.5
+    MU2, SIGMA2 = 3, 2.2
 
-plt.show()
+    # data
+    X1 = np.random.normal(MU1, SIGMA1, N)
+    X2 = np.random.normal(MU2, SIGMA2, N)
+
+    # calculate propagated error from means, sigma
+    PROP_X1 = 1 / np.mean(X2)**2 * SIGMA1**2
+    PROP_X2 = np.mean(X1)**2 / np.mean(X2)**4 * SIGMA2**2
+    PROP = PROP_X1 + PROP_X2
+
+    # output
+    print("mean X1: %s" % (np.mean(X1)))
+    print("mean X2: %s" % (np.mean(X2)))
+    print("mean X1/X2: %s" % (np.mean(X1/X2)))
+    print("stddev X1: %s" % (np.std(X1)))
+    print("stddev X2: %s" % (np.std(X2)))
+    print("stddev X1/X2: %s" % (np.std(X1/X2)))
+    print("propagated uncertainty (sq): %s" % (PROP))
+    print("propagated uncertainty (rt): %s" % (np.sqrt(PROP)))
+
+    # plot
+    plt.hist(X1/X2, 50, normed=True)
+    plt.show()
