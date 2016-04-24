53d4c1448fb29eba4aa9077130c1a0bb8d43b51c
mrbaozi <mrbaozi@me.com>
2016-04-24 07:19:02 +0200

    Debian sync

---
+++ sheet2/a1_2.py
@@ -0,0 +1,10 @@
+import sys
+import numpy as np
+
+if __name__ == "__main__":
+    try:
+        N = int(sys.argv[1])
+    except IndexError:
+        N = 100
+
+    FORECAST = np.random.uniform(size=N)
---
+++ sheet2/a2_2.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+
+if __name__ == "__main__":
+    NPOINTS = 10
+    XOF = 10
+
+    X = [x for x in range(XOF, XOF + NPOINTS, 1)]
+    Y = [y for y in range(XOF, XOF + NPOINTS, 1)]
+    EX = [np.random.normal(0, 0.5) for _ in range(NPOINTS)]
+    EY = [np.random.normal(0, 0.5) for _ in range(NPOINTS)]
+
+    b, a, r_value, p_value, std_err = stats.linregress(X + EX,Y + EY)
+    print(b)
+    print(a)
+    print(r_value)
+    print(p_value)
+    print(std_err)
