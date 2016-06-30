3cdd0cd91d6869ea1e6a33827be9978b53c3bfd1
mrbaozi <mrbaozi@me.com>
2016-06-30 17:15:35 +0200

    finish ex10

--- sheet9/ex10_template.py
+++ sheet9/ex10_template.py
@@ -25,7 +25,7 @@ data = np.loadtxt('iris.data')
 # Define dictionary with columns names: s=sepal, p=petal, l=lenght, w=width,
 flowers = {'setosa', 'versicolor', 'virginica'}
 columns = {0: 'L(Kelch)', 1: 'W(Kelch)',
-           2: 'L(Blatt)', 3: 'W(Blatt)', 4: 'class'}
+        2: 'L(Blatt)', 3: 'W(Blatt)', 4: 'class'}
 
 # Define boolean arrays corresponding to the three
 #      classes setosa, versicolor and virginica
@@ -34,7 +34,8 @@ versicolor = data[:, 4] == 1
 virginica = data[:, 4] == 2
 
 # Signal is versicolor (can be changed to setosa or virginica)
-signal = versicolor
+ndim = 2
+signal = setosa
 bckgrd = ~signal
 # !! note: see indexing of arrays with boolenan array in pyhthon documentation
 #
@@ -66,9 +67,9 @@ class Plotter(object):
         ydim = 1
         # Draw the scatter-plots of signal and background
         plt.scatter(self.signal_data[:, xdim], self.signal_data[:, ydim],
-          c='r', label='Signal')
+                c='r', label='Signal')
         plt.scatter(self.bckgrd_data[:, xdim], self.bckgrd_data[:, ydim],
-          c='b', label='Background')
+                c='b', label='Background')
 
         # Evaluate the response function on a two-dimensional grid ...
         #   ... using the mean-values of the data for the remaining dimensions.
@@ -93,20 +94,20 @@ class Plotter(object):
             vector = classifier.fisher_vector / np.linalg.norm(classifier.fisher_vector)
             plt.axes().set_aspect('equal', 'datalim')
             plt.plot([-vector[xdim]+means[xdim], vector[xdim]+means[xdim]],
-                          [-vector[ydim]+means[ydim], vector[ydim]+means[ydim]],
-                          'k-', lw=4, label="Fisher Projection")
-        plt.title(
-          "scatter plot {} vs {} and classifier contour".format(
-          columns[xdim], columns[ydim] ) )
-        #cbar.draw_all()
+                    [-vector[ydim]+means[ydim], vector[ydim]+means[ydim]],
+                    'k-', lw=4, label="Fisher Projection")
+            plt.title(
+                    "scatter plot {} vs {} and classifier contour".format(
+                        columns[xdim], columns[ydim] ) )
+                    #cbar.draw_all()
         plt.show()
 
     def plot_test_statistic(self, classifier):
         # Draw Distribution of the test-statistic
         ns, binss, _ = plt.hist(list(map(classifier.evaluate, self.signal_data)),
-          color='r', alpha=0.5, label='Signal' )
+                color='r', alpha=0.5, label='Signal' )
         nb, binsb, _ = plt.hist( list(map(classifier.evaluate, self.bckgrd_data)),
-           color='b', alpha=0.5, label='Background' )
+                color='b', alpha=0.5, label='Background' )
         plt.title("test statistic")
         plt.show()
 
@@ -166,23 +167,20 @@ class CutClassifier(object):
         self.bckgrd_cov = np.cov(bckgrd_data.T)
 
     def evaluate(self, x):
-    # simple example of a cut-base classifier
+        # simple example of a cut-base classifier
         c=0
         for i in range(len(x)):
-           c+=(x[i] < (self.signal_mean[i] + self.bckgrd_mean[i])/2.)
+            c+=(x[i] < (self.signal_mean[i] + self.bckgrd_mean[i])/2.)
         return c
 
-# example how to use a Classifier Class with the Plotter Class
-ndim = 2
-
 # initialise Classifier with training data
-# cut = CutClassifier()
-# cut.fit(data[signal, :ndim], data[bckgrd, :ndim])
+cut = CutClassifier()
+cut.fit(data[signal, :ndim], data[bckgrd, :ndim])
 
 # initialize Plotter Class
-# plotter = Plotter(data[signal, :ndim], data[bckgrd, :ndim])
+plotter = Plotter(data[signal, :ndim], data[bckgrd, :ndim])
 # and amke plots
-# plotter.plot_contour(cut)
+plotter.plot_contour(cut)
 # plotter.plot_test_statistic(cut)
 # plotter.plot_roc(cut)
 
@@ -215,53 +213,116 @@ plt.show()
 #Exercise 2
 
 class LikelihoodClassifier(object):
+
     def fit(self, signal_data, bckgrd_data):
-        """
-            set up classifier ("training")
-        """
-    # some examples of what might be useful:
-      # 1. signal and background histograms with same binning
-        _, self.edges = np.histogramdd(np.vstack([signal_data, bckgrd_data]), bins=10)
+
+        _, self.edges = np.histogramdd(np.vstack([signal_data, bckgrd_data]), bins=2)
         self.signal_hist, _ = np.histogramdd(signal_data, bins=self.edges)
         self.bckgrd_hist, _ = np.histogramdd(bckgrd_data, bins=self.edges)
 
-      # 2. mean and covariance matrix
-        self.signal_mean = np.mean(signal_data, axis=0)
-        self.signal_cov = np.cov(signal_data.T)
-        self.bckgrd_mean = np.mean(bckgrd_data, axis=0)
-        self.bckgrd_cov = np.cov(bckgrd_data.T)
-
     def evaluate(self, x):
-    # simple example of a cut-base classifier
-        c=0
+
+        point = np.zeros_like(x, dtype=int)
         for i in range(len(x)):
-           c+=(x[i] < (self.signal_mean[i] + self.bckgrd_mean[i])/2.)
-        return c
+            point[i] = find_bin(x[i], self.edges[i])
+        return self.signal_hist[tuple(point)] / self.bckgrd_hist[tuple(point)]
+        # return 1 if self.signal_hist[tuple(point)] / self.bckgrd_hist[tuple(point)] > 0 else -1
 
-# example how to use a Classifier Class with the Plotter Class
-ndim = 2
+lh = LikelihoodClassifier()
+lh.fit(data[signal, :ndim], data[bckgrd, :ndim])
 
-# initialise Classifier with training data
-# llh = LikelihoodClassifier()
-# llh.fit(data[signal, :ndim], data[bckgrd, :ndim])
-
-# initialize Plotter Class
-# plotter = Plotter(data[signal, :ndim], data[bckgrd, :ndim])
-# and make plots
-# plotter.plot_contour(llh)
+plotter = Plotter(data[signal, :ndim], data[bckgrd, :ndim])
+plotter.plot_contour(lh)
 # plotter.plot_test_statistic(llh)
 # plotter.plot_roc(llh)
 
 #------------------------------------------------------------------
 # Exercise 3
 
+def gaussdd(x, mu, sig):
+    t = np.zeros_like(x)
+    for i in range(len(x)):
+        t[i] = ((x[i] - mu[i])**2 / (2 * sig[i]**2))
+    return np.exp(-(np.sum(t)))
+
+class LogLikelihoodClassifier(object):
+
+    def fit(self, signal_data, bckgrd_data):
+
+        _, self.edges = np.histogramdd(np.vstack([signal_data, bckgrd_data]), bins=10)
+        self.signal_hist, _ = np.histogramdd(signal_data, bins=self.edges)
+        self.bckgrd_hist, _ = np.histogramdd(bckgrd_data, bins=self.edges)
+
+        self.signal_mean = np.mean(signal_data, axis=0)
+        self.signal_cov = np.cov(signal_data.T)
+        self.signal_stddev = np.sqrt(np.diag(self.signal_cov))
+        self.bckgrd_mean = np.mean(bckgrd_data, axis=0)
+        self.bckgrd_cov = np.cov(bckgrd_data.T)
+        self.bckgrd_stddev = np.sqrt(np.diag(self.bckgrd_cov))
+
+    def evaluate(self, x):
+
+        p_signal = gaussdd(x, self.signal_mean, self.signal_stddev)
+        p_bckgrd = gaussdd(x, self.bckgrd_mean, self.bckgrd_stddev)
+
+        # prior_signal = 1/3
+        # prior_bckgrd = 2/3
+        # post_signal = prior_signal * np.prod(p_signal)
+        # post_bckgrd = prior_bckgrd * np.prod(p_bckgrd)
+        post_signal = np.prod(p_signal)
+        post_bckgrd = np.prod(p_bckgrd)
+
+        return np.log(post_signal / post_bckgrd)
+        # return 1 if np.log(post_signal / post_bckgrd) > 0 else -1
+
+llh = LogLikelihoodClassifier()
+llh.fit(data[signal, :ndim], data[bckgrd, :ndim])
+
+plotter = Plotter(data[signal, :ndim], data[bckgrd, :ndim])
+plotter.plot_contour(llh)
+# plotter.plot_test_statistic(llh)
+# plotter.plot_roc(llh)
 
 # ------------------------------------------------------------------
 # Exercise 4
 
+class FisherLinearClassifier(object):
 
-# ------------------------------------------------------------------
-# Exercise 5
-#   -> Set ndim to 4
+    def fit(self, signal_data, bckgrd_data):
 
+        _, self.edges = np.histogramdd(np.vstack([signal_data, bckgrd_data]), bins=10)
+        self.signal_hist, _ = np.histogramdd(signal_data, bins=self.edges)
+        self.bckgrd_hist, _ = np.histogramdd(bckgrd_data, bins=self.edges)
 
+        self.signal_data = signal_data
+        self.bckgrd_data = bckgrd_data
+        self.signal_mean = np.mean(signal_data, axis=0)
+        self.bckgrd_mean = np.mean(bckgrd_data, axis=0)
+
+        # calculate fisher weights which maximize linear separation
+        self.signal_del = signal_data - self.signal_mean
+        self.bckgrd_del = bckgrd_data - self.bckgrd_mean
+        self.var = np.dot(self.signal_del.T, self.signal_del) + np.dot(self.bckgrd_del.T, self.bckgrd_del)
+        self.fish = np.dot(np.linalg.inv(self.var), (self.bckgrd_mean - self.signal_mean))
+
+    def evaluate(self, x):
+        return np.dot(x, self.fish) / np.linalg.norm(self.fish)
+        # return 1 if np.dot(x, self.fish) / np.linalg.norm(self.fish) > 0 else -1
+
+    def plot_separation(self):
+        sig = np.dot(self.signal_data, self.fish)
+        bg = np.dot(self.bckgrd_data, self.fish)
+
+        fg, ax = plt.subplots(1, 1)
+        ax.plot(sig, np.zeros_like(sig), 'ro')
+        ax.plot(bg, np.zeros_like(bg), 'go')
+        plt.show()
+
+fsh = FisherLinearClassifier()
+fsh.fit(data[signal, :ndim], data[bckgrd, :ndim])
+fsh.plot_separation()
+
+plotter = Plotter(data[signal, :ndim], data[bckgrd, :ndim])
+plotter.plot_contour(fsh)
+# plotter.plot_test_statistic(llh)
+# plotter.plot_roc(llh)
